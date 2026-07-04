55f93963569cd6487c36d43e2dc0ff7b638e29af
00peter0 <peterpavelka888@gmail.com>
2026-07-04 17:58:27 +0000

    Use notification-safe Virtu launcher mask

--- ui/src/main/java/com/wireguard/android/VpnRouterService.kt
+++ ui/src/main/java/com/wireguard/android/VpnRouterService.kt
@@ -131,7 +131,7 @@ class VpnRouterService : Service() {
             Notification.Builder(this)
         }
         return builder
-            .setSmallIcon(R.mipmap.ic_launcher)
+            .setSmallIcon(R.drawable.ic_stat_virtu)
             .setColor(Color.BLACK)
             .setContentTitle(getString(R.string.vcs_vpn_router_service_title))
             .setContentText(text)
