aa607a88fbb7a95c2ca83e4840ccbdfe937716c7
00peter0 <peterpavelka888@gmail.com>
2026-07-06 13:47:48 +0000

    Replace browser widgets with search bar widget

--- ui/src/main/java/com/wireguard/android/widget/SecuredBrowserWidgetProvider.kt
+++ ui/src/main/java/com/wireguard/android/widget/SecuredBrowserWidgetProvider.kt
@@ -9,7 +9,7 @@ import android.widget.RemoteViews
 import com.wireguard.android.R
 import com.wireguard.android.activity.SecureBrowserActivity
 
-open class SecuredBrowserWidgetProvider : AppWidgetProvider() {
+class SecuredBrowserWidgetProvider : AppWidgetProvider() {
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         appWidgetIds.forEach { appWidgetId ->
             val layoutId = appWidgetManager.getAppWidgetInfo(appWidgetId)?.initialLayout
@@ -23,12 +23,8 @@ open class SecuredBrowserWidgetProvider : AppWidgetProvider() {
                 PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
             )
             views.setOnClickPendingIntent(R.id.widget_root, launchIntent)
-            if (layoutId == R.layout.widget_secured_browser_status) {
-                views.setOnClickPendingIntent(R.id.widget_button, launchIntent)
-            }
+            views.setOnClickPendingIntent(R.id.widget_button, launchIntent)
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
     }
 }
-
-class SecuredBrowserStatusWidgetProvider : SecuredBrowserWidgetProvider()
